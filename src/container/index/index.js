24d21b79a898a10dd1cfbfe3f3ecad2709742319
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2024-05-21 21:18:06 +0300

    have some troubles with page

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,11 @@
-import '../../script/test'
+import {createElement, createHeader} from '../../script/layout'
 
-console.log('container')
+const page = document.querySelector('.page')
+
+const header = createHeader()
+
+page.append(header)
+
+const title = createElement('h1', 'title', 'Мій блог')
+
+page.append(title)
