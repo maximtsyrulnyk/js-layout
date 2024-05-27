e3ac3bf82fc4113d9f1a443a4f012dba7a62765b
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2024-05-27 16:31:37 +0300

    make some changes in file script/layout.js according last recommendation

--- src/container/community/index.js
+++ src/container/community/index.js
@@ -1,5 +1,4 @@
-import { PathPosix } from 'path-scurry'
-import {createElement, createHeader} from '../../script/layout'
+import {createElement, createHeader, createContainerTab} from '../../script/layout'
 
 const page = document.querySelector('.page')
 
@@ -7,80 +6,19 @@ const header = createHeader()
 
 page.append(header)
 
-const title = createElement('h1', 'title', 'Мій блог')
+const title = createElement('h1', 'title', 'Коммьюніті')
 
 page.append(title)
 
+const containerTab = createContainerTab()
+page.append(containerTab)
 
 const POST_LIST = [
-    {
-        category: [
-            {text: 'Важливо',  id: 1},
-            {text: 'Нова', id: 2},
-        ],
-        info: 'До біса планувальник, наймаємо дизайнера і готуємося до презентації, як Джобс',
-        date: '25.01',
-        viewed: false,
-    },
-    {
-        category: [{text: 'Нова', id: 2}],
-        info: 'Ми хотіли щоб у цьому чаті було близько 150 людей щоб зробити якісний пак самопрезентацій.',
-        date: '24.01',
-        viewed: true,
-    },
+   
 ]
 
 const createPost = () => {
-    const postList = createElement('main', 'post__list')
 
-    POST_LIST.forEach((postData) => {
-        const post = createElement(
-            'div',
-            postData.viewed
-            ? 'post button post--viewed'
-            : 'post button',
-        )
-
-        const postHeader = createElement('div', 'post__header')
-
-        const categoryList = createElement(
-            'div',
-            'post__category-list',
-        )
-
-        postData.category.forEach((category) => {
-            const categorySpan = createElement(
-                'span',
-                `post__category post__category--${category.id}`,
-                category.text,
-            )
-            categoryList.append(categorySpan)
-        })
-
-        // ====
-
-        const dateSpan = createElement(
-            'span',
-            'post__date',
-            postData.date,
-        )
-        postHeader.append(categoryList, dateSpan)
-
-        // ====
-
-        const infoParagraph = createElement(
-            'p',
-            'post__info',
-            postData.info,
-        )
-        post.append(postHeader, infoParagraph)
-
-        // ====
-
-        postList.append(post)
-    })
-
-    return postList
 }
 
 // ===
--- src/script/layout.js
+++ src/script/layout.js
@@ -1,70 +1,74 @@
 export const createElement = (tag, className, text) => {
     const elem = document.createElement(tag)
-
-    if(className) {
-        elem.className = className
+  
+    if (className) {
+      elem.className = className
     }
-    if(text) {
-        elem.innerHTML = text
+  
+    if (text) {
+      elem.innerHTML = text
     }
-
+  
     return elem
-}
-
-const HEADER_BUTTON_LIST = [
+  }
+  
+  const HEADER_BUTTON_LIST = [
     {
-        width: 24,
-        height: 24,
-        src: '/svg/header__back.svg',
+      width: 24,
+      height: 24,
+      src: '/svg/header__back.svg',
     },
     {
-        width: 24,
-        height: 24,
-        src: '/img/header__avatar.png',
-    }
-]
-
-export const createHeader = () => {
+      width: 24,
+      height: 24,
+      src: '/img/header__avatar.png',
+    },
+  ]
+  
+  const TAB_BUTTON_LIST = [
+    {
+      active: false,
+      info: 'База знань'
+  
+    },
+    {
+      active: true,
+      info: 'Інформація'
+    },
+  ]
+  
+  export const createHeader = () => {
     const header = createElement('header', 'header')
-
+  
     HEADER_BUTTON_LIST.forEach((params) => {
-        const button = createElement('button', 'button')
-
-        const img = createElement('img')
-
-        Object.entries(params).forEach(([key, value]) => {
-            img[key] = value
-        })
-
-        button.insertAdjacentElement('beforeend', img)
-
-        header.insertAdjacentElement('beforeend', button)
+      const button = createElement('button', 'button')
+  
+      const img = createElement('img')
+  
+      Object.entries(params).forEach(([key, value]) => {
+        img[key] = value
+      })
+  
+      button.insertAdjacentElement('beforeend', img)
+  
+      header.insertAdjacentElement('beforeend', button)
     })
-
+  
     return header
-    }
-    const TAB_BUTTON_LIST = [
-        {
-          active: false,
-          info: 'База знань'
-      
-        },
-        {
-          active: true,
-          info: 'Інформація'
-        },
-      ]
-export const createContainerTab = () => {
+  }
+  
+  
+  
+  export const createContainerTab = () => {
     const div = createElement('div', 'tab__list')
-
+  
     TAB_BUTTON_LIST.forEach((params) => {
-        const tab = createElement('div', params.active ? 'button tab__button tab__active' : 'button tab__button')
-
-        tab.innerText = params.info
-
-        div.insertAdjectedElement('beforeend', tab)
+      const tab = createElement('div', params.active ? 'button tab__button tab__active' : 'button tab__button')
+  
+      tab.innerText = params.info
+  
+      div.insertAdjacentElement('beforeend', tab)
     })
-
+  
     return div
-}
-    
+  }
