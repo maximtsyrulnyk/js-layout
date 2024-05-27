6b321d0b86ce7cefc865d89411125843f99d4ca4
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2024-05-27 19:37:29 +0300

    done hometask to JS practice Layout through JS lesson

--- src/container/community/index.js
+++ src/container/community/index.js
@@ -25,3 +25,34 @@ const createPost = () => {
 
 const post = createPost()
 page.append(post)
+
+const MAIN_INFO = {
+    img: '/img/community_img.png',
+    title: 'Що таке база знань?',
+    text: 'База знаний — база данных, содержащая правила вывода и информацию о человеческом опыте и знаниях в некоторой предметной области. В самообучающихся системах база знаний также содержит информацию, являющуюся результатом решения предыдущих задач. ',
+    button: `Перейти до ком'юніті у Телеграм`, 
+}
+
+const createMain = () => {
+    const main = createElement('main', 'main');
+
+    const img = createElement('img', 'main__img');
+    img.src = MAIN_INFO.img;
+    main.appendChild(img);
+
+    const title = createElement('div', 'main__title', MAIN_INFO.title);
+    main.appendChild(title);
+
+    const textContainer = createElement('div', 'main__text');
+    const paragraph = createElement('p', 'main__paragraph', MAIN_INFO.text);
+    textContainer.appendChild(paragraph);
+    main.appendChild(textContainer);
+
+    const button = createElement('button', 'main__button', MAIN_INFO.button);
+    main.appendChild(button);
+
+    return main;
+};
+
+const containerMain = createMain()
+page.append(containerMain);
